5c7b5c2a16a143955119ed1b4a95de39174513e5
Keivan Shahida <keivanshahida@icloud.com>
2019-10-04 11:35:45 -0400

    Add GeoSpark SQL and GeoSpark Viz
    Signed-off-by: Keivan Shahida <keivanshahida@icloud.com>

--- geomesa-geospark/src/test/scala/org/locationtech/geomesa/geospark/TestEnvironment.scala
+++ geomesa-geospark/src/test/scala/org/locationtech/geomesa/geospark/TestEnvironment.scala
@@ -14,25 +14,25 @@ import org.apache.spark.sql._
 /**
  * Common JTS test setup and utilities.
  */
-trait TestEnvironment {
-  implicit lazy val spark: SparkSession = {
-    SparkSession.builder()
-      .appName("testSpark")
-      .master("local[*]")
-      .getOrCreate()
-      .withJTS
-  }
+//trait TestEnvironment {
+//  implicit lazy val spark: SparkSession = {
+////    SparkSession.builder()
+////      .appName("testSpark")
+////      .master("local[*]")
+////      .getOrCreate()
+////      .withJTS
+//  }
 
-  lazy val sc: SQLContext = spark.sqlContext
-    .withJTS // <-- this should be a noop given the above, but is here to test that code path
+//  lazy val sc: SQLContext = spark.sqlContext
+//    .withJTS // <-- this should be a noop given the above, but is here to test that code path
 
   /**
    * Constructor for creating a DataFrame with a single row and no columns.
    * Useful for testing the invocation of data constructing UDFs.
    */
-  def dfBlank(implicit spark: SparkSession): DataFrame = {
-    // This is to enable us to do a single row creation select operation in DataFrame
-    // world. Probably a better/easier way of doing this.
-    spark.createDataFrame(spark.sparkContext.makeRDD(Seq(Row())), StructType(Seq.empty))
-  }
+//  def dfBlank(implicit spark: SparkSession): DataFrame = {
+//    // This is to enable us to do a single row creation select operation in DataFrame
+//    // world. Probably a better/easier way of doing this.
+//    spark.createDataFrame(spark.sparkContext.makeRDD(Seq(Row())), StructType(Seq.empty))
+//  }
 }
