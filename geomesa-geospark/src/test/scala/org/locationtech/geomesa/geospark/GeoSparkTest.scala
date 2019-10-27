5f6fbbb3fee7ee894d4b6e12b35069a6b509075c
Keivan Shahida <keivanshahida@icloud.com>
2019-10-27 13:36:47 -0400

    Convert dataframe to Geomesa RDD
    Signed-off-by: Keivan Shahida <keivanshahida@icloud.com>

--- geomesa-geospark/src/test/scala/org/locationtech/geomesa/geospark/GeoSparkTest.scala
+++ geomesa-geospark/src/test/scala/org/locationtech/geomesa/geospark/GeoSparkTest.scala
@@ -8,14 +8,18 @@
 
 package org.locationtech.geomesa.geospark
 
-import org.locationtech.jts.geom._
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.datasyslab.geospark
+import org.geotools.data.{DataStoreFinder, Query, Transaction}
 import org.junit.runner.RunWith
+import org.locationtech.jts.geom._
+import org.opengis.feature.simple.SimpleFeature
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
-import org.datasyslab.geospark
 
 @RunWith(classOf[JUnitRunner])
 class GeoSparkTest extends Specification with TestEnvironment {
@@ -41,64 +45,104 @@ class GeoSparkTest extends Specification with TestEnvironment {
       .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ").load(dataFile)
     df.createOrReplaceTempView("inputtable")
     df.show()
-
-
-    //    JTS VERSION
-//    val dataFile = this.getClass.getClassLoader.getResource("jts-example.csv").getPath
-//    df = spark.read.schema(schema)
-//      .option("sep", "-")
-//      .option("timestampFormat", "yyyy/MM/dd HH:mm:ss ZZ")
-//      .csv(dataFile)
   }
+
+  "geomesa rdd" should {
+    "read from dataframe" in {
+      val geomesaRDD = df.rdd
+      geomesaRDD.collect()
+
+      geomesaRDD.count mustEqual(df.count)
+    }
+
+    // Example row: [itemA,Point (40 40),Polygon ((35 35, 45 35, 45 45, 35 45, 35 35)),40,40]
+    "have rows with user defined types" in {
+      val geomesaRDD = df.rdd
+      geomesaRDD.collect().foreach(println)
+
+      val row = geomesaRDD.first()
+      true mustEqual(true)
+    }
+  }
+
+  "geospark rdd" should {
+    "read from geomesa rdd" in {
+
+      true mustEqual(true)
+    }
+  }
+
 
   "spark jts module" should {
 
     "have rows with user defined types" >> {
 
-      spark.sql(
-        """
-          |SELECT *
-          |FROM inputtable
-        """.stripMargin).show()
+//      val params = Map(
+//        "geotools" -> "true",
+//        "file"     -> "jts-example.csv")
+//      val query = new Query("locations")
+//      val rdd = GeoMesaSpark(params).rdd(new Configuration(), sc, params, query)
 
-      newDF = spark.sql(
-        """
-          |SELECT ST_PointFromText(_c1,' ') AS pointshape
-          |FROM inputtable
-    """.stripMargin)
+//            spark.sql(
+//              """
+//                |SELECT *
+//                |FROM inputtable
+//              """.stripMargin).show()
 
-      newDF.show()
+      //      newDF = spark.sql(
+      //        """
+      //          |SELECT ST_PointFromText(_c1,' ') AS pointshape
+      //          |SELECT ST_PolygonFromText(_c2, ' ') AS polygonshape
+      //          |SELECT ST_PointFromText(
+      //          |FROM inputtable
+      //    """.stripMargin)
 
-//      JTS VERSION
-//      newDF = df.withColumn("point", st_pointFromText(col("pointText")))
-//        .withColumn("polygon", st_polygonFromText(col("polygonText")))
-//        .withColumn("pointB", st_makePoint(col("latitude"), col("longitude")))
-//
-//      newDF.createOrReplaceTempView("example")
-//      val row = newDF.first()
-//      val gf = new GeometryFactory
-//      row.get(5).isInstanceOf[Point] mustEqual true
-//      row.get(6).isInstanceOf[Polygon] mustEqual true
-//      row.get(7).isInstanceOf[Point] mustEqual true
+      //      newDF = spark.sql(
+      //        """
+      //          |SELECT ST_Point(cast(inputtable._c3 as Decimal(24,20)),cast(inputtable._c4 as Decimal(24,20))) as shape
+      //          |FROM inputtable
+      //    """.stripMargin)
+      //      newDF.createOrReplaceTempView("newdf")
+      //      newDF.show()
+
+      //      newDF = spark.sql(
+      //        """
+      //          |SELECT ST_GeomFromWKT(_c2) AS polygonshape
+      //          |FROM inputtable
+      //    """.stripMargin)
+      //      newDF.createOrReplaceTempView("newdf")
+      //      newDF.show()
+
+      //      JTS VERSION
+      //      newDF = df.withColumn("point", st_pointFromText(col("pointText")))
+      //        .withColumn("polygon", st_polygonFromText(col("polygonText")))
+      //        .withColumn("pointB", st_makePoint(col("latitude"), col("longitude")))
+      //
+      //      newDF.createOrReplaceTempView("example")
+      //      val row = newDF.first()
+      //      val gf = new GeometryFactory
+      //      row.get(5).isInstanceOf[Point] mustEqual true
+      //      row.get(6).isInstanceOf[Polygon] mustEqual true
+      //      row.get(7).isInstanceOf[Point] mustEqual true
       true mustEqual(true)
     }
 
     "create a df from sequence of points" >> {
 
-//      JTS VERSION
-//      val points = newDF.collect().map{r => r.getAs[Point](5)}
-//      val testDF = spark.createDataset(points).toDF()
-//      testDF.count() mustEqual df.count()
+      //      JTS VERSION
+      //      val points = newDF.collect().map{r => r.getAs[Point](5)}
+      //      val testDF = spark.createDataset(points).toDF()
+      //      testDF.count() mustEqual df.count()
       true mustEqual(true)
     }
 
-//    "udfs intergrate with dataframe api" >> {
-//      val countSQL = sc.sql("select * from example where st_contains(st_makeBBOX(0.0, 0.0, 90.0, 90.0), point)").count()
-//      val countDF = newDF
-//        .where(st_contains(st_makeBBOX(lit(0.0), lit(0.0), lit(90.0), lit(90.0)), col("point")))
-//        .count()
-//      countSQL mustEqual countDF
-//    }
+    //    "udfs intergrate with dataframe api" >> {
+    //      val countSQL = sc.sql("select * from example where st_contains(st_makeBBOX(0.0, 0.0, 90.0, 90.0), point)").count()
+    //      val countDF = newDF
+    //        .where(st_contains(st_makeBBOX(lit(0.0), lit(0.0), lit(90.0), lit(90.0)), col("point")))
+    //        .count()
+    //      countSQL mustEqual countDF
+    //    }
   }
 
   // after
@@ -106,3 +150,4 @@ class GeoSparkTest extends Specification with TestEnvironment {
     spark.stop()
   }
 }
+
